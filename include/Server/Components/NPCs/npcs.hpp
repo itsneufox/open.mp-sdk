c46eb87dd55da52ec8510fe7247b46b98479940d
itsneufox <156133096+itsneufox@users.noreply.github.com>
2026-05-29 01:43:13 +0100

    Add NPC node link callback

--- include/Server/Components/NPCs/npcs.hpp
+++ include/Server/Components/NPCs/npcs.hpp
@@ -453,6 +453,7 @@ struct NPCEventHandler
 	virtual void onNPCPlaybackEnd(INPC& npc, int recordId) { }
 	virtual void onNPCFinishNodePoint(INPC& npc, int nodeId, uint16_t pointId) { }
 	virtual void onNPCFinishNode(INPC& npc, int nodeId) { }
+	virtual bool onNPCSelectNodeLink(INPC& npc, int nodeId, uint16_t pointId, uint16_t linkId, int targetNodeId, uint16_t targetPointId, uint32_t targetFlags, uint8_t targetFloodFill) { return true; }
 	virtual bool onNPCChangeNode(INPC& npc, int newNodeId, int oldNodeId) { return true; }
 	virtual void onNPCFinishMovePathPoint(INPC& npc, int pathId, int pointId) { }
 	virtual void onNPCFinishMovePath(INPC& npc, int pathId) {};
